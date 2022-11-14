956e86947fb563787aab7b21b084ae49592eb9c9
K H <himmi.khaleed@gmail.com>
2022-11-14 22:46:27 +0000

    V01 commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = User::create([
+            'name' => 'User Test', 
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('12345678')
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    echo "<center> Welcome </center>";
+});
+
+$router->get('/version', function () use ($router) {
+    return $router->app->version();
+});
+
+Route::group(['prefix' => 'api'], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('user-profile', 'AuthController@me');
+});
